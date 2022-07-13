e6c826133a342d56dd21b5eb0be4c28dd8c7b563
Dogpa <dogpa0130@gmail.com>
2022-07-13 12:44:23 +0800

    Initial Commit

---
+++ Swift Practice 165 Same ViewModel In Different View/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Swift Practice 165 Same ViewModel In Different View
+//
+//  Created by Dogpa's MBAir M1 on 2022/7/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Swift Practice 165 Same ViewModel In Different View/Swift_Practice_165_Same_ViewModel_In_Different_ViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_Practice_165_Same_ViewModel_In_Different_ViewApp.swift
+//  Swift Practice 165 Same ViewModel In Different View
+//
+//  Created by Dogpa's MBAir M1 on 2022/7/13.
+//
+
+import SwiftUI
+
+@main
+struct Swift_Practice_165_Same_ViewModel_In_Different_ViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
